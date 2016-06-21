54fb813fb82858c5df330e0931b3e9dcb4ad666d
Hedy Wang <hedywings@gmail.com>
2016-06-22 00:14:44 +0800

    readme modify

--- index.js
+++ index.js
@@ -10,53 +10,83 @@ var fs = require('fs'),
 /*************************************************************************************************/
 /*** Loading Enumerations                                                                      ***/
 /*************************************************************************************************/
-DEFS.ipsoCharUuid = new Enum(_defs.ipsoCharUuid);
+_defs.oid = new Enum(_defs.oid);
+_defs.ipsoCharUuid = new Enum(_defs.ipsoCharUuid);
+
 
 /*************************************************************************************************/
 /*** DEFS Methods                                                                              ***/
 /*************************************************************************************************/
-DEFS.getUuid = function (oid) {
-    if (typeof oid !== 'string' || typeof oid !== 'number')
+DEFS.ou = function (oid) {
+    if (typeof oid !== 'string' && typeof oid !== 'number')
         throw new Error('oid must be a string or number');
 
-    var oidNum = oid.parseInt(),
+    var oidNum = parseInt(oid),
         oidName;
 
     if (!isNaN(oidNum))
         oid = oidNum;
 
-    oidName = _defs.oid.get(oid).key;
+    oidName = _defs.oid.get(oid);
 
     if (!oidName) {
         return;
     } else {
-        return DEFS.ipsoCharUuid.get(oidName).value;
+        return '0x' + _defs.ipsoCharUuid.get(oidName.key).value.toString(16);
     }
 };
 
-DEFS.getParams = function (uuid) {
-    var params = { mandatory: [], optional: [] },
-        paramObj;
-
+DEFS.uo = function (uuid) {
     if (typeof uuid !== 'string' && typeof uuid !== 'number')
         throw new Error('uuid must be a string or number');
 
-    if (typeof uuid === 'number')
-        uuid = '0x' + uuid.toString(16);
+    var uuidNum = parseInt(uuid),
+        oidName;
 
-    paramObj = _defs[uuid].params;
+    if (!isNaN(uuidNum))
+        uuid = uuidNum;
+
+    oidName = _defs.ipsoCharUuid.get(uuid);
+
+    if (!oidName) {
+        return;
+    } else {
+        return oidName.key;
+    }
+};
+
+DEFS.spec = function (id) {
+    var paramObj,
+        defs = { 
+            oid: null,
+            uuid: null,
+            fields: {
+                mandatory: [], 
+                optional: [] 
+            }
+        };
+
+    if (typeof id !== 'string')
+        throw new Error('id must be a string');
+
+    if (!id.startsWith('0x'))
+        id = this.ou(id);
+
+    defs.uuid = id,
+    defs.oid = this.uo(id);
+    paramObj = _defs[id].params;
 
     paramObj.mandatory.forEach(function (item, idx) {
         var name = Object.keys(item)[0];
-        params.mandatory.push({name: name, type: item[name]});
+        defs.fields.mandatory.push({name: name, type: item[name]});
     });
 
     paramObj.optional.forEach(function (item, idx) {
         var name = Object.keys(item)[0];
-        params.optional.push({name: name, type: item[name]});
+        defs.fields.optional.push({name: name, type: item[name]});
     });
 
-    return params;
+    return defs;
 };
 
 module.exports = DEFS;
